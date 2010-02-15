f23d90b1ba89a342671285b9766ffd80cd5b8299
Maxim Kremenev <ezotrank@gmail.com>
2010-02-15 13:08:02 +0200

    Add product downloadable link to admin_product_tabs

--- downloadable_hooks.rb
+++ downloadable_hooks.rb
@@ -3,4 +3,11 @@ class DownloadableHooks < Spree::ThemeSupport::HookListener
     "<tr><td><%= link_to t(\"downloadable_settings\"), admin_downloadable_settings_path %></td><td>
      <%= t(\"downloadable_settings_description\") %></td></tr>"
   end
+  
+  insert_after :admin_product_tabs do
+    "<li<%= \' class=\"active\"\' if current == \"Downloadables\" %>>
+     <%= link_to t(\"product_files\"), admin_product_downloadables_path(@product) %>
+     </li>"
+  end
+  
 end
