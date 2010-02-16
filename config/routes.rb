1151f62a186884b7dd116b69d511deaea453ac63
Maxim Kremenev <ezotrank@gmail.com>
2010-02-16 21:55:49 +0200

    delete some old staff from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
-# Put your extension routes here.
-
 map.namespace :admin do |admin|
-  admin.resources :products, :has_many => [:downloadables] do |product|
-    # product.create_bundle 'create_bundle/:id', :controller => 'downloadables', :action => 'create_bundle'
-    # product.update_product_download_limit 'update_product_download_limit/:id', :controller => 'downloadables', 
-    #                                       :action => 'update_product_download_limit'
-  end
+  admin.resources :products, :has_many => [:downloadables]
   admin.resource :downloadable_settings
 end  
 
