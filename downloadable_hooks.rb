8e6d9473ebb566edec6ba0ff824e439adb0f21b7
Maxim Kremenev <ezotrank@gmail.com>
2010-02-16 12:56:58 +0200

    Add comment

--- downloadable_hooks.rb
+++ downloadable_hooks.rb
@@ -3,7 +3,10 @@ class DownloadableHooks < Spree::ThemeSupport::HookListener
     "<tr><td><%= link_to t(\"downloadable_settings\"), admin_downloadable_settings_path %></td><td>
      <%= t(\"downloadable_settings_description\") %></td></tr>"
   end
-  
+
+  # Delete variants from admin product tabs
+  # TODO: letter try to many varinants for one product, for example:
+  # Original film has a many format - mov, avi and else
   replace :admin_product_tabs, 'admin/shared/download_tabs'
   
   # When product has a downloadables we render some slightly different
