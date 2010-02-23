6e6cb17c73348ee3d15ccb4d49a577134d126815
Maxim Kremenev <ezotrank@gmail.com>
2010-02-24 00:34:01 +0200

    add old product option

--- downloadable_hooks.rb
+++ downloadable_hooks.rb
@@ -5,16 +5,9 @@ class DownloadableHooks < Spree::ThemeSupport::HookListener
      <%= t(\"downloadable_settings_description\") %></td></tr>"
   end
 
-  # Delete variants from admin product tabs
-  # TODO: letter try to many varinants for one product, for example:
-  # Original film has a many format - mov, avi and else
-  # replace :admin_product_tabs, 'admin/shared/download_tabs'
   insert_after :admin_product_tabs, 'admin/shared/download_tabs'
   
-  # Delete none need field from product admin edit
-  replace :admin_product_form_right, 'admin/products/downloable_product_form'
-  
-  # When product has a downloadables we render some slightly different
+    # When product has a downloadables we render some slightly different
   # html templates
   # replace :inside_product_cart_form, 'shared/show_price'
   # replace :cart_item_quantity, 'shared/cart_item_quanity'
