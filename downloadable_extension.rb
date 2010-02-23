25e84e7427c5afc042424aa82c716a490ca83795
Maxim Kremenev <ezotrank@gmail.com>
2010-02-24 01:16:59 +0200

    fix send option(on mail)

--- downloadable_extension.rb
+++ downloadable_extension.rb
@@ -18,12 +18,9 @@ class DownloadableExtension < Spree::Extension
     
     Product.class_eval do 
       has_many :downloadables, :as => :viewable, :order => :position, :dependent => :destroy
-      
       def downlodables?
         true if !self.downloadables.empty?
       end
-      
-      # named_scope :available, :conditions => ['products.available_on <= ?', Time.zone.now]
     end
     
     Variant.class_eval do 
@@ -31,17 +28,11 @@ class DownloadableExtension < Spree::Extension
     end
     
     LineItem.class_eval do 
-      before_update :fix_quantity
       before_create :add_default_staff
-      
-      # Fuck, it's ugly, but I don't know how use it before_update {self.quantity = 1}
-      def fix_quantity
-        self.quantity = 1
-      end
       
       # Add some staff to line_item when it's create
       def add_default_staff
-        self.download_code = random_password
+        self.download_code = random_password if self.variant.product.downlodables?
       end
       
       # Check if link is not die
@@ -66,33 +57,24 @@ class DownloadableExtension < Spree::Extension
       default_url_options[:host] = Spree::Config[:site_url]
     end
     
-    ShippingMethod.class_eval do
-      class << self
-        # Use thid method to shipping_method_id
-        def download
-          self.find_by_name('Download')
-        end
-      end
-    end
-    
     ApplicationHelper.class_eval do
       # Checks if checkout cart has ONLY downloadable items
       # Used for shipping in helpers/checkouts_helper.rb
-      def only_downloadable
-        downloadable_count = 0
-        @order.line_items.each do |item|
-          if((!item.product.downloadables.empty?) || (!item.variant.downloadables.empty?))
-            downloadable_count += 1
-          end
-        end
-        @order.line_items.size == downloadable_count
-      end
+      # def only_downloadable
+      #   downloadable_count = 0
+      #   @order.line_items.each do |item|
+      #     if((!item.product.downloadables.empty?) || (!item.variant.downloadables.empty?))
+      #       downloadable_count += 1
+      #     end
+      #   end
+      #   @order.line_items.size == downloadable_count
+      # end
       
-      def has_downloadable?
-        @order.line_items.each do |item|
-          return true if ((!item.product.downloadables.empty?) || (!item.variant.downloadables.empty?))
-        end
-      end
+      # def has_downloadable?
+      #   @order.line_items.each do |item|
+      #     return true if ((!item.product.downloadables.empty?) || (!item.variant.downloadables.empty?))
+      #   end
+      # end
       
       def generate_secret(record)
         Digest::MD5.hexdigest("#{record.id}-#{ActionController::Base.session_options[:secret]}")
